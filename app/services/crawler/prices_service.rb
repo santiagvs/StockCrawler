080f86de0e2c6f9f365ac12b3a69e4b92e0fdeb8
Santiago <victor.lacsilva0102@gmail.com>
2022-11-13 20:00:18 -0300

    feat: now returns a hash with stock data

--- app/services/crawler/prices_service.rb
+++ app/services/crawler/prices_service.rb
@@ -1,12 +1,28 @@
 module Crawler
   class PricesService
-    BASE_URL = 'https://valorinveste.globo.com/cotacoes/'.freeze
+    class << self
+      BASE_URL = 'https://valorinveste.globo.com/cotacoes/'.freeze
 
-    def initialize(price_code)
-      response = HTTParty.get(BASE_URL)
-      match_row(price_code, response)
+      def run(symbol)
+        response = HTTParty.get(BASE_URL)
+        stock_data = fetch_symbol_data(symbol, response)
+        parse_action_info(stock_data)
+      end
+
+      def fetch_symbol_data(symbol, response)
+        document ||= Nokogiri::HTML(response.body)
+        rows = document.css("tbody.vd-table__body tr[contains('#{symbol}')] td.table-date-value")
+        return nil if rows.empty?
+
+        rows.map { |row| row.text.squish }
+      end
+
+      def parse_action_info(stock_data)
+        data = []
+        columns = %i[name code last change last_day]
+        currency = columns.zip(stock_data).to_h
+        data << currency
+      end
     end
-
-    def match_row(price_code, response); end
   end
 end
